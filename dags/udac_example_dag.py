fff6bf344a27694cb3408cd1b9fe1e75058d499f
Anders Thirsgaard Rasmussen <atr@mir-robots.com>
2021-04-12 16:29:27 +0200

    DAG configuration

--- dags/udac_example_dag.py
+++ dags/udac_example_dag.py
@@ -12,6 +12,12 @@ from helpers import SqlQueries
 default_args = {
     'owner': 'udacity',
     'start_date': datetime(2019, 1, 12),
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'catchup': False
 }
 
 dag = DAG('udac_example_dag',
